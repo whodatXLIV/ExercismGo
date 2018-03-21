46bb87895e9f21c5c3016a35793a20240ffc6308
Seth_PC <sethamin.7@gmail.com>
2018-03-21 14:58:45 -0700

    Passed tests

--- bob/bob.go
+++ bob/bob.go
@@ -1,15 +1,33 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package bob provides Bob's responses to conversation
 package bob
 
-// Hey should have a comment documenting it.
+import "strings"
+
+// Hey returns a string of Bob's response to remark
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+
+	remark = strings.TrimSpace(remark)
+	if len(remark) == 0 {
+		//Remark is silence
+		return "Fine. Be that way!"
+	}
+
+	upperRemark := strings.ToUpper(remark)
+	isWords := strings.ContainsAny(upperRemark, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	isQuestion := strings.HasSuffix(remark, "?")
+	isYell := remark == upperRemark && isWords
+
+	switch {
+	case isQuestion && isYell: //Remark is a yelled question
+		return "Calm down, I know what I'm doing!"
+
+	case isQuestion: // Remark is a question
+		return "Sure."
+
+	case isYell: //Remark is yelled
+		return "Whoa, chill out!"
+
+	default: // default
+		return "Whatever."
+	}
 }
